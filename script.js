3b37e8c75be6a5e7a3d2265ca1cac02651171d18
lmontem <lmontem7@gmail.com>
2020-11-24 15:52:56 -0500

    event listener for like button

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ let editButton = document.querySelector('.profile__edit-btn');
 let editCloseBtn = editProfilePopup.querySelector('.popup__close-btn');
 let addCardBtn = document.querySelector('.profile__add-btn');
 let closeAddBtn = addCardPopup.querySelector('.popup__close-btn');
-let likeBtn = document.querySelector('.card__like-btn');
-let deleteBtn = document.querySelector('.card__delete-btn');
+
+
 //edit form
 let editForm = editProfilePopup.querySelector('.popup__form');
 let name = editProfilePopup.querySelector('.popup__input_type_name');
@@ -29,63 +29,74 @@ let title = cardTemplate.querySelector('.card__heading');
 let image = cardTemplate.querySelector('.card__image');
 
 let initialCards = [
-    {
-      name: "Yosemite Valley",
-      link: "https://code.s3.yandex.net/web-code/yosemite.jpg"
-    },
-    {
-      name: "Old Faithful",
-      link: "https://images.unsplash.com/photo-1600269450099-58dd5976074a"
-    },
-    {
-      name: "Bald Mountains",
-      link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg"
-    },
-    {
-      name: "Niagra Falls",
-      link: "https://images.unsplash.com/photo-1463695970743-ae65cca05743"
-    },
-    {
-      name: "Grand Canyon",
-      link: "https://images.unsplash.com/photo-1527333656061-ca7adf608ae1"
-    },
-    {
-      name: "Letchworth State Park",
-      link: "https://images.unsplash.com/photo-1518719668060-f16ad0ce7602"
-    }
-  ];
-//adds intial cards 
+  {
+    name: "Yosemite Valley",
+    link: "https://code.s3.yandex.net/web-code/yosemite.jpg"
+  },
+  {
+    name: "Old Faithful",
+    link: "https://images.unsplash.com/photo-1600269450099-58dd5976074a"
+  },
+  {
+    name: "Bald Mountains",
+    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg"
+  },
+  {
+    name: "Niagra Falls",
+    link: "https://images.unsplash.com/photo-1463695970743-ae65cca05743"
+  },
+  {
+    name: "Grand Canyon",
+    link: "https://images.unsplash.com/photo-1527333656061-ca7adf608ae1"
+  },
+  {
+    name: "Letchworth State Park",
+    link: "https://images.unsplash.com/photo-1518719668060-f16ad0ce7602"
+  }
+];
+//popup open/close function
+function formOpenClose(popup) {
+  popup.classList.toggle('popup_opened');
+}
 
-initialCards.forEach((card)=>{
+//add new card
+function addCard(newCard) {
   let cardElem = cardTemplate.cloneNode(true);
   let title = cardElem.querySelector('.card__heading');
   let image = cardElem.querySelector('.card__image');
-    
-    title.textContent = card.name;
-    image.style.backgroundImage = `url(${card.link})`;    
-    cards.prepend(cardElem);
+
+  let likeBtn = cardElem.querySelector('.card__like-btn');
+//let deleteBtn = cardTemplate.querySelector('.card__delete-btn');
+
+  title.textContent = newCard.name;
+  image.style.backgroundImage = `url(${newCard.link})`;
+  cards.prepend(cardElem);
+
+  //like button event
+  likeBtn.addEventListener('click', (e) => {
+    e.target.classList.toggle('card__like-btn_active');
+  })
 
   //open image popup
-    image.addEventListener('click', ()=>{
+  image.addEventListener('click', () => {
     let popupImage = imagePopup.querySelector('.popup__image');
     let popupImageTitle = imagePopup.querySelector('.popup__image-title');
-    popupImage.src = card.link;
-    popupImageTitle.textContent = card.name;
+    popupImage.src = newCard.link;
+    popupImageTitle.textContent = newCard.name;
     formOpenClose(imagePopup);
-  })
-  })
 
-  function formOpenClose(popup) {     
-    popup.classList.toggle('popup_opened');    
+  })
 }
 
-
+//adds initial cards
+initialCards.forEach(card => addCard(card));
 
 //open and close add card popup
-addCardBtn.addEventListener('click', ()=>{
-  formOpenClose(addCardPopup);  
+addCardBtn.addEventListener('click', () => {
+
+  formOpenClose(addCardPopup);
 })
-closeAddBtn.addEventListener('click', ()=>{
+closeAddBtn.addEventListener('click', () => {
   formOpenClose(addCardPopup);
 })
 
@@ -93,42 +104,40 @@ closeAddBtn.addEventListener('click', ()=>{
 //functonality of adding images/title
 let addFormUpdate = e => {
   e.preventDefault();
-  let cardElem = cardTemplate.cloneNode(true);
-  let title = cardElem.querySelector('.card__heading');
-  let image = cardElem.querySelector('.card__image');
-    
-    title.textContent = cardTitle.value;
-    image.style.backgroundImage = `url(${cardLink.value})`;    
-    cards.prepend(cardElem);
-  formOpenClose(addCardPopup);  
+
+  let cardElem = [];
+  cardElem.name = cardTitle.value;
+  cardElem.link = cardLink.value;
+  addCard(cardElem);
+  formOpenClose(addCardPopup);
 }
 
-addCardForm.addEventListener('submit',addFormUpdate)
+addCardForm.addEventListener('submit', addFormUpdate)
 
 
 
 //saves changes edit profile form
-function editFormChange(event){
-    event.preventDefault();   
+function editFormChange(event) {
+  event.preventDefault();
 
-    profileName.textContent = name.value;
-    profileOccupation.textContent = aboutMe.value;
+  profileName.textContent = name.value;
+  profileOccupation.textContent = aboutMe.value;
 
-   formOpenClose(editProfilePopup);   
+  formOpenClose(editProfilePopup);
 }
-editForm.addEventListener('submit',editFormChange);
+editForm.addEventListener('submit', editFormChange);
 //opens profile edit popup
-editButton.addEventListener('click', ()=>{
-  if (!editProfilePopup.classList.contains('popup_opened')){
+editButton.addEventListener('click', () => {
+  if (!editProfilePopup.classList.contains('popup_opened')) {
     name.value = profileName.textContent;
     aboutMe.value = profileOccupation.textContent;
-}
-formOpenClose(editProfilePopup);
+  }
+  formOpenClose(editProfilePopup);
 });
 //closes profile edit popup
-editCloseBtn.addEventListener('click', ()=>{
+editCloseBtn.addEventListener('click', () => {
   formOpenClose(editProfilePopup);
- 
+
 });
 
 
