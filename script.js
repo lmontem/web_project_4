a1fc5aac465e69acd4d8f0b9354285ea853d1af4
lmontem <lmontem7@gmail.com>
2020-11-25 10:52:04 -0500

    image popup and transition

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 //popups
 let imagePopup = document.querySelector('.popup__type_image');
 let editProfilePopup = document.querySelector('.popup__type_edit-profile');
@@ -10,7 +9,7 @@ let editButton = document.querySelector('.profile__edit-btn');
 let editCloseBtn = editProfilePopup.querySelector('.popup__close-btn');
 let addCardBtn = document.querySelector('.profile__add-btn');
 let closeAddBtn = addCardPopup.querySelector('.popup__close-btn');
-
+let imageClosebtn = imagePopup.querySelector('.popup__close-btn');
 
 //edit form
 let editForm = editProfilePopup.querySelector('.popup__form');
@@ -28,93 +27,99 @@ let cards = document.querySelector('.cards');
 let title = cardTemplate.querySelector('.card__heading');
 let image = cardTemplate.querySelector('.card__image');
 
-let initialCards = [
-  {
-    name: "Yosemite Valley",
-    link: "https://code.s3.yandex.net/web-code/yosemite.jpg"
-  },
-  {
-    name: "Old Faithful",
-    link: "https://images.unsplash.com/photo-1600269450099-58dd5976074a"
-  },
-  {
-    name: "Bald Mountains",
-    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg"
-  },
-  {
-    name: "Niagra Falls",
-    link: "https://images.unsplash.com/photo-1463695970743-ae65cca05743"
-  },
-  {
-    name: "Grand Canyon",
-    link: "https://images.unsplash.com/photo-1527333656061-ca7adf608ae1"
-  },
-  {
-    name: "Letchworth State Park",
-    link: "https://images.unsplash.com/photo-1518719668060-f16ad0ce7602"
-  }
+let initialCards = [{
+        name: "Yosemite Valley",
+        link: "https://code.s3.yandex.net/web-code/yosemite.jpg"
+    },
+    {
+        name: "Old Faithful",
+        link: "https://images.unsplash.com/photo-1600269450099-58dd5976074a"
+    },
+    {
+        name: "Bald Mountains",
+        link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg"
+    },
+    {
+        name: "Niagra Falls",
+        link: "https://images.unsplash.com/photo-1463695970743-ae65cca05743"
+    },
+    {
+        name: "Grand Canyon",
+        link: "https://images.unsplash.com/photo-1527333656061-ca7adf608ae1"
+    },
+    {
+        name: "Letchworth State Park",
+        link: "https://images.unsplash.com/photo-1518719668060-f16ad0ce7602"
+    }
 ];
 //popup open/close function
 function formOpenClose(popup) {
-  popup.classList.toggle('popup_opened');
+    popup.classList.toggle('popup_opened');
+
 }
 
 //add new card
 function addCard(newCard) {
-  let cardElem = cardTemplate.cloneNode(true);
-  let title = cardElem.querySelector('.card__heading');
-  let image = cardElem.querySelector('.card__image');
+    let cardElem = cardTemplate.cloneNode(true);
+    let title = cardElem.querySelector('.card__heading');
+    let image = cardElem.querySelector('.card__image');
 
-  let likeBtn = cardElem.querySelector('.card__like-btn');
-let deleteBtn = cardElem.querySelector('.card__delete-btn');
+    let likeBtn = cardElem.querySelector('.card__like-btn');
+    let deleteBtn = cardElem.querySelector('.card__delete-btn');
 
-  title.textContent = newCard.name;
-  image.style.backgroundImage = `url(${newCard.link})`;
-  cards.prepend(cardElem);
+    title.textContent = newCard.name;
+    image.style.backgroundImage = `url(${newCard.link})`;
+    cards.prepend(cardElem);
 
-  //like button event
-  likeBtn.addEventListener('click', (e) => {
-    e.target.classList.toggle('card__like-btn_active');
-  })
+    //like button event
+    likeBtn.addEventListener('click', (e) => {
+        e.target.classList.toggle('card__like-btn_active');
+    })
 
-  //delete button event
-  deleteBtn.addEventListener('click', ()=>{
-    cardElem.remove(newCard);
-  })
+    //delete button event
+    deleteBtn.addEventListener('click', () => {
+        cardElem.remove(newCard);
+    })
 
-  //open image popup
-  image.addEventListener('click', () => {
-    let popupImage = imagePopup.querySelector('.popup__image');
-    let popupImageTitle = imagePopup.querySelector('.popup__image-title');
-    popupImage.src = newCard.link;
-    popupImageTitle.textContent = newCard.name;
-    formOpenClose(imagePopup);
+    //open image popup
+    image.addEventListener('click', () => {
+        let popupImage = imagePopup.querySelector('.popup__image');
+        let popupImageTitle = imagePopup.querySelector('.popup__image-title');
+        popupImage.src = newCard.link;
+        popupImageTitle.textContent = newCard.name;
+        formOpenClose(imagePopup);
+    })
 
-  })
+
 }
 
 //adds initial cards
 initialCards.forEach(card => addCard(card));
 
+//image popup close button event
+imageClosebtn.addEventListener('click', () => {
+    formOpenClose(imagePopup);
+});
+
 //open and close add card popup
 addCardBtn.addEventListener('click', () => {
 
-  formOpenClose(addCardPopup);
+    formOpenClose(addCardPopup);
 })
 closeAddBtn.addEventListener('click', () => {
-  formOpenClose(addCardPopup);
+    formOpenClose(addCardPopup);
 })
 
 
 //functonality of adding images/title
 let addFormUpdate = e => {
-  e.preventDefault();
+    e.preventDefault();
 
-  let cardElem = [];
-  cardElem.name = cardTitle.value;
-  cardElem.link = cardLink.value;
-  addCard(cardElem);
-  formOpenClose(addCardPopup);
+    let cardElem = [];
+    cardElem.name = cardTitle.value;
+    cardElem.link = cardLink.value;
+    addCard(cardElem);
+    formOpenClose(addCardPopup);
 }
 
 addCardForm.addEventListener('submit', addFormUpdate)
@@ -123,27 +128,24 @@ addCardForm.addEventListener('submit', addFormUpdate)
 
 //saves changes edit profile form
 function editFormChange(event) {
-  event.preventDefault();
+    event.preventDefault();
 
-  profileName.textContent = name.value;
-  profileOccupation.textContent = aboutMe.value;
+    profileName.textContent = name.value;
+    profileOccupation.textContent = aboutMe.value;
 
-  formOpenClose(editProfilePopup);
+    formOpenClose(editProfilePopup);
 }
 editForm.addEventListener('submit', editFormChange);
 //opens profile edit popup
 editButton.addEventListener('click', () => {
-  if (!editProfilePopup.classList.contains('popup_opened')) {
-    name.value = profileName.textContent;
-    aboutMe.value = profileOccupation.textContent;
-  }
-  formOpenClose(editProfilePopup);
+    if (!editProfilePopup.classList.contains('popup_opened')) {
+        name.value = profileName.textContent;
+        aboutMe.value = profileOccupation.textContent;
+    }
+    formOpenClose(editProfilePopup);
 });
 //closes profile edit popup
 editCloseBtn.addEventListener('click', () => {
-  formOpenClose(editProfilePopup);
+    formOpenClose(editProfilePopup);
 
 });
-
-
-
