088a798e3dfca1565a53b7a0f97cca04c65aca75
lmontem <lmontem7@gmail.com>
2021-01-07 12:03:08 -0500

    refractor/section-class

---
+++ scripts/Section.js
@@ -0,0 +1,16 @@
+class Section {
+    constructor({ items, renderer }, classSelector) {
+        this._array = items;
+        this._renderer = renderer;
+        this._container = document.querySelector(classSelector);
+    }
+    renderItems() {
+        this._array.forEach(item => {
+            this._renderer(item);
+        });
+
+    }
+    addItem(element) {
+        this._container.append(element);
+    }
+}
