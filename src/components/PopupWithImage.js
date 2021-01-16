183b3ff0f30b6b8ff3100b026bec0d2d72c8d1fe
lmontem <lmontem7@gmail.com>
2021-01-15 19:09:30 -0500

    refractor/ popup w/ image

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -3,11 +3,13 @@ import Popup from './Popup.js';
 class PopupWithImage extends Popup {
     constructor(popupSelector) {
         super(popupSelector);
+        this._source = this._popupElement.querySelector('.popup__image');
+        this._info =  this._popupElement.querySelector('.popup__image-title');
     }
     open(caption, link) {
         super.open();
-        this._popupElement.querySelector('.popup__image').src = link;
-        this._popupElement.querySelector('.popup__image-title').textContent = caption;
+        this._source.src = link;
+        this._info.textContent = caption;
     }
 }
 
